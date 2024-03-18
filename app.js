49bb660c9548439fdc9d88c09a12a7f1e1004b2c
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-18 23:10:46 +0530

    Request and Response

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http')
 
-const server =http.createServer((res, req)=>{
-    console.log(req)
-    console.log("Kuldeep")
+const server =http.createServer((req, res)=>{
+    console.log(req.url , req.headers , req.method)
+    // console.log("Kuldeep")
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Hello from Node.js server!</h1></body>')
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(4000)
